918d2f4508f8783af6c9a55d0e9290e622ff2710
MichaelSani05 <msmichaelsani@gmail.com>
2024-12-15 22:04:32 +0100

    V-1.1.0

--- src/app/components/lista-ofertas-solicitudes/lista-ofertas-solicitudes.component.ts
+++ src/app/components/lista-ofertas-solicitudes/lista-ofertas-solicitudes.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, QueryList, ViewChildren, ElementRef } from '@angular/core';
 import { DataService } from '../../services/datos-firebase.service';
 import { CommonModule } from '@angular/common';
 
@@ -12,8 +12,12 @@ import { CommonModule } from '@angular/common';
 export class ListaOfertasComponent implements OnInit{
   @Input() tabla: any = ''
   items: any[] = [];
+  capacidadesExperiencia: any[] = [];
+  uniquePoblaciones: string[] = [];
   oferta: boolean = false
   solicitud: boolean = false
+  poblacion: string = ''
+  @ViewChildren('poblacionOption') poblacionOption!: QueryList<ElementRef>;
 
   constructor(private dataService: DataService) {
     
@@ -23,19 +27,71 @@ export class ListaOfertasComponent implements OnInit{
     this.dataService.getItems(this.tabla).subscribe({
       next: (data) => {
         this.items = data;
+        this.capacidadesExperiencia = data;
+
+        if (this.tabla === 'ofertas') {
+          this.uniquePoblaciones = [...new Set(data.map((item: any) => item.poblacion2))];
+        } else if (this.tabla === 'solicitudes') {
+          this.uniquePoblaciones = [...new Set(data.map((item: any) => item.poblacion))];
+        }
       },
       error: (error) => {
         console.error('Error al recuperar datos', error);
       }
     });
 
-    if (this.tabla == 'ofertas') {
-      this.oferta = true
-      this.solicitud = false
-    } else if (this.tabla == 'solicitudes') {
-      this.oferta = false
-      this.solicitud = true
+    if (this.tabla === 'ofertas') {
+      this.oferta = true;
+      this.solicitud = false;
+      this.poblacion = "poblacion2"
+    } else if (this.tabla === 'solicitudes') {
+      this.oferta = false;
+      this.solicitud = true;
+      this.poblacion = "poblacion"
     }
 
   }
+
+  onSelect(event: Event): void {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+
+    if (!selectedValue) {
+      this.dataService.getItems(this.tabla).subscribe({
+        next: (data) => {
+          this.capacidadesExperiencia = data;
+        },
+        error: (error) => {
+          console.error('Error al recuperar datos', error);
+        }
+      });
+    } else {
+      this.dataService.getFilteredItems(this.tabla, this.poblacion, selectedValue).subscribe({
+        next: (data) => {
+          this.capacidadesExperiencia = data;
+        },
+        error: (error) => {
+          console.error('Error al recuperar datos', error);
+        }
+      });
+    }
+    console.log(selectedValue);
+  }
+
+  onSearch(event: Event): void {
+    const searchTerm = (event.target as HTMLInputElement).value.toLowerCase();
+
+    if (searchTerm != '' && this.solicitud == true) {
+      this.capacidadesExperiencia = this.items.filter((solicitud) =>
+        solicitud.capacidades.toLowerCase().includes(searchTerm)
+      );
+    } else if (searchTerm != '' && this.oferta == true) {
+      this.capacidadesExperiencia = this.items.filter((solicitud) =>
+        solicitud.requerimientos.toLowerCase().includes(searchTerm)
+      );
+    } 
+    else {
+      this.capacidadesExperiencia = [...this.items];
+    }
+  }
+
 }
--- src/app/services/datos-firebase.service.ts
+++ src/app/services/datos-firebase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Database, ref, onValue, query, orderByChild } from '@angular/fire/database';
+import { Database, ref, onValue, query, orderByChild, equalTo } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -29,25 +29,29 @@ export class DataService {
     });
   }
 
-  getFilteredItems(tabla: string, filtro: string): Observable<any[]> {
+  getFilteredItems(tabla: string, filtroKey: string, filtroValue: string): Observable<any[]> {
     const dbRef = ref(this.database, tabla);
-    const filteredQuery = query(dbRef, orderByChild(filtro));
-
+    const filteredQuery = query(dbRef, orderByChild(filtroKey), equalTo(filtroValue));
+  
     return new Observable((observer) => {
-      onValue(filteredQuery, (snapshot) => {
-        const data = snapshot.val();
-        const items: any[] = [];
-        
-        if (data) {
-          Object.keys(data).forEach(key => {
-            items.push({ id: key, ...data[key] });
-          });
+      onValue(
+        filteredQuery,
+        (snapshot) => {
+          const data = snapshot.val();
+          const items: any[] = [];
+  
+          if (data) {
+            Object.keys(data).forEach((key) => {
+              items.push({ id: key, ...data[key] });
+            });
+          }
+  
+          observer.next(items);
+        },
+        (error) => {
+          observer.error(error);
         }
-        
-        observer.next(items);
-      }, (error) => {
-        observer.error(error);
-      });
+      );
     });
   }
 }
