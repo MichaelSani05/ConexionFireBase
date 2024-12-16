f8d1f326e1e9609020fa61bb09c0b28a9bde95eb
MichaelSani05 <msmichaelsani@gmail.com>
2024-12-16 13:02:57 +0100

    V-1.1.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { MainComponent } from './pages/main/main.component';
 import { FormulariosComponent } from './pages/formularios/formularios.component';
 import { ConsultarOfertasComponent } from './pages/consultar-ofertas/consultar-ofertas.component';
 import { ConsultarSolicitudesComponent } from './pages/consultar-solicitudes/consultar-solicitudes.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     {path: "", component: MainComponent},
     {path: "Formularios", component: FormulariosComponent},
     {path: "ConsultarOfertas", component: ConsultarOfertasComponent},
     {path: "ConsultarSolicitudes", component: ConsultarSolicitudesComponent},
+    {path: "Login", component: LoginComponent},
 ];
